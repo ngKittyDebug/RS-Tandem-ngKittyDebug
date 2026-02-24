6d386d76a1f86fb00954789ce97beecc737d3e65
Whale Joy <afterlifelineonomega@gmail.com>
2026-02-24 20:10:30 +0300

    feat: add pages, routes initials, update prettier setup

--- frontend/eslint.config.cjs
+++ frontend/eslint.config.cjs
@@ -3,6 +3,7 @@ const eslint = require('@eslint/js');
 const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const prettier = require('eslint-config-prettier');
 
 module.exports = defineConfig([
   {
@@ -15,6 +16,7 @@ module.exports = defineConfig([
       tseslint.configs.recommended,
       tseslint.configs.stylistic,
       angular.configs.tsRecommended,
+      prettier,
     ],
     processor: angular.processInlineTemplates,
     rules: {
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/main/main').then((m) => m.Main),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'registration',
+    loadComponent: () => import('./pages/registration/registration').then((m) => m.Registration),
+  },
+  {
+    path: 'user-profile',
+    loadComponent: () => import('./pages/user-profile/user-profile').then((m) => m.UserProfile),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound),
+  },
+];
