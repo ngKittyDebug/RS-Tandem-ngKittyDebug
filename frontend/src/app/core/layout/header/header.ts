c2e26e9d7d50cb63cb0fd1fe6a5ff299731d2a1e
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-02-27 14:41:35 +0300

    feat: add basic header layout styles

--- frontend/src/app/core/layout/header/header.ts
+++ frontend/src/app/core/layout/header/header.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TuiHeader } from '@taiga-ui/layout';
 import { LaguageSwitcher } from '../../../core/components/laguage-switcher/laguage-switcher';
 import { ThemeSwitcher } from '../../components/theme-switcher/theme-switcher';
+import { TuiAppearance } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-header',
-  imports: [TuiHeader, LaguageSwitcher, ThemeSwitcher],
+  imports: [TuiHeader, TuiAppearance, LaguageSwitcher, ThemeSwitcher],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
