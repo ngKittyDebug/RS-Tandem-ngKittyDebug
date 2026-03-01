0efa30deb5072fea029b1939bd77dcce5bc22f54
kozochkina82 <kozochkina82@yandex.ru>
2026-03-01 01:28:19 +0100

    feat: add page 404 content

--- frontend/src/app/pages/not-found/not-found.ts
+++ frontend/src/app/pages/not-found/not-found.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-not-found',
   imports: [],
+  standalone: true,
   templateUrl: './not-found.html',
-  styleUrl: './not-found.scss',
+  styleUrls: ['./not-found.scss],
 })
 export class NotFound {}
