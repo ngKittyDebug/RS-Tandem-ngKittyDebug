767d09e3d5489b4a7c7c85ef38a28c3e8a95fa46
kozochkina82 <kozochkina82@yandex.ru>
2026-03-01 22:44:42 +0100

    feat: add link to main page

--- frontend/src/app/pages/not-found/not-found.ts
+++ frontend/src/app/pages/not-found/not-found.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { TuiLink } from '@taiga-ui/core';
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [TuiLink],
   standalone: true,
   templateUrl: './not-found.html',
-  styleUrls: ['./not-found.scss],
+  styleUrls: ['./not-found.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFound {}
