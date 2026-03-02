2a630042577969910b2c132a5a2531032212f734
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-03-02 18:54:45 +0700

    feat: add basic layout and styles for registration page

--- frontend/src/app/pages/registration/registration.ts
+++ frontend/src/app/pages/registration/registration.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-import { TranslocoDirective } from '@jsverse/transloco';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiAppearance, TuiButton, TuiTextfield } from '@taiga-ui/core';
+import {} from '@taiga-ui/kit';
+import { TuiCardLarge, TuiForm } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-registration',
-  imports: [TranslocoDirective],
   templateUrl: './registration.html',
   styleUrl: './registration.scss',
+  standalone: true,
+  imports: [TuiTextfield, TuiButton, TuiAppearance, TuiCardLarge, TuiForm],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Registration {}
