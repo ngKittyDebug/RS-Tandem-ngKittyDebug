1aa5ea3c15cbd7bcc3f987983380e0049adec101
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-03-02 21:57:09 +0700

    feat: add form logic

--- frontend/src/app/pages/registration/registration.ts
+++ frontend/src/app/pages/registration/registration.ts
@@ -1,14 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TuiAppearance, TuiButton, TuiTextfield } from '@taiga-ui/core';
 import {} from '@taiga-ui/kit';
 import { TuiCardLarge, TuiForm } from '@taiga-ui/layout';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+// import { PASSWORD_PATTERN } from 'src/app/shared/validators/password.pattern'; TODO когда Алена замержит свой компонент а я его спулю, это надо раскоментировать и проверить путь!
+// import { EMAIL_PATTERN } from 'src/app/shared/validators/email.pattern'; TODO когда Алена замержит свой компонент а я его спулю, это надо раскоментировать и проверить путь!
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.html',
-  styleUrl: './registration.scss',
+  styleUrls: ['./registration.scss'],
   standalone: true,
-  imports: [TuiTextfield, TuiButton, TuiAppearance, TuiCardLarge, TuiForm],
+  imports: [TuiTextfield, TuiButton, TuiAppearance, TuiCardLarge, TuiForm, ReactiveFormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Registration {}
+export class Registration {
+  public registrationForm: FormGroup;
+  private fb = inject(FormBuilder);
+
+  constructor() {
+    this.registrationForm = this.fb.group({
+      name: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          // Validators.pattern(EMAIL_PATTERN), TODO когда Алена замержит свой компонент а я его спулю, это надо раскоментировать  и проверить путь!
+        ],
+      ],
+      password: [
+        '',
+        Validators.required,
+        // Validators.pattern(PASSWORD_PATTERN), TODO когда Алена замержит свой компонент а я его спулю, это надо раскоментировать  и проверить путь!
+      ],
+      passwordRepeat: ['', Validators.required],
+    });
+  }
+
+  public submit(): void {
+    alert('Сабмит работает!');
+  }
+}
