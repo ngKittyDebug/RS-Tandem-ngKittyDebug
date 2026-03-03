e57d1199809370b73cabf77591eeee4179a48a36
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-03-04 04:17:57 +0700

    feat: submit registration data to backend

--- frontend/src/app/pages/registration/registration.ts
+++ frontend/src/app/pages/registration/registration.ts
@@ -4,8 +4,12 @@ import {} from '@taiga-ui/kit';
 import { TuiCardLarge, TuiForm } from '@taiga-ui/layout';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { TranslocoService, TranslocoModule } from '@jsverse/transloco';
-// import { PASSWORD_PATTERN } from 'src/app/shared/validators/password.pattern'; TODO когда Алена замержит свой компонент а я его спулю, это надо раскоментировать и проверить путь!
-// import { EMAIL_PATTERN } from 'src/app/shared/validators/email.pattern'; TODO когда Алена замержит свой компонент а я его спулю, это надо раскоментировать и проверить путь!
+import { passwordsValidator } from './validators';
+import { AuthService } from '../../core/services/register-service';
+import { PASSWORD_PATTERN } from '../../core/patterns/password-pattern';
+import { EMAIL_PATTERN } from '../../core/patterns/email-pattern';
+import { RegisterDto } from './models/register.interfaces';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -26,30 +30,67 @@ import { TranslocoService, TranslocoModule } from '@jsverse/transloco';
 export class Registration {
   private translocoService = inject(TranslocoService);
   private fb = inject(FormBuilder);
-
+  private authService = inject(AuthService);
   public t(key: string): string {
     return this.translocoService.translate(key);
   }
-  public registrationForm = this.fb.group({
-    name: ['', [Validators.required]],
-    email: [
-      '',
-      [
-        Validators.required,
-        // Validators.pattern(EMAIL_PATTERN), TODO когда Алена замержит свой компонент, а я его спулю, это надо раскомментировать и проверить путь!
-      ],
-    ],
-    password: [
-      '',
-      [
-        Validators.required,
-        // Validators.pattern(PASSWORD_PATTERN), TODO когда Алена замержит свой компонент, а я его спулю, это надо раскомментировать и проверить путь!
-      ],
-    ],
-    passwordRepeat: ['', [Validators.required]],
-  });
+  public registrationForm = this.fb.group(
+    {
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern(EMAIL_PATTERN)]],
+      password: ['', [Validators.required, Validators.pattern(PASSWORD_PATTERN)]],
+      passwordRepeat: ['', [Validators.required]],
+    },
+    {
+      validators: passwordsValidator,
+    },
+  );
 
-  public submit(): void {
-    alert('Сабмит работает!');
+  public async submit(): Promise<void> {
+    const { username, email, password } = this.registrationForm.getRawValue();
+    if (!username || !email || !password) throw new Error('Что-то не так');
+    const User: RegisterDto = {
+      username: username,
+      email: email,
+      password: password,
+    };
+    console.log(User);
+    try {
+      await firstValueFrom(this.authService.register(User));
+      console.log('Регистрация прошла успешно!');
+    } catch (error) {
+      console.error(error);
+      console.log('Произошла ошибка при регистрации');
+    }
+    // const respons = await fetch('https://meow-vault-pr-44.onrender.com/auth/register', { method: "POST", body: JSON.stringify(User) })
+    // const data = await respons.json();
+    // console.log(data)
   }
+  //   public async simulateNetworkError(): Promise<void> {
+  //     // Запрос на несуществующий домен вызовет HttpErrorResponse с status 0 (сетевая ошибка)
+  //     const url = 'https://meow-vault-pr-44.onrender.com/auth/register';
+  //     const { username, email, password } = this.registrationForm.getRawValue();
+  //     const User: RegisterDto = {
+  //       "email": "user@example.com",
+  //       "username": "john_doe",
+  //       "password": "StrongPass123!"
+  //     };
+  //     try {
+  //       const response = await fetch(url, {
+  //         method: 'POST',
+  //         headers: {
+  //           'Content-Type': 'application/json' // Обязательно для JSON
+  //         },
+  //         body: JSON.stringify(User) // Преобразование объекта в строку
+  //       });
+
+  //       const result = await response.json();
+  //       console.log(result);
+  //       const label = result.error;
+  //       const message = result.message.toString();
+  //     } catch (error) {
+  //       console.error('Ошибка запроса:', error);
+  //     }
+
+  //   }
 }
