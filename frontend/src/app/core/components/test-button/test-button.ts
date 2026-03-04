adcd725d8d132156e4a87e03a7837a5de6a9bad4
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-03-04 08:05:04 +0300

    feat: add basic error toster

---
+++ frontend/src/app/core/components/test-button/test-button.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { AppErrorService } from '../../services/app-error-service';
+
+@Component({
+  selector: 'app-test-button',
+  imports: [],
+  templateUrl: './test-button.html',
+  styleUrl: './test-button.scss',
+})
+export class TestButton {
+  public errorService = inject(AppErrorService);
+
+  public async simulateNetworkError(): Promise<void> {
+    // Запрос на несуществующий домен вызовет HttpErrorResponse с status 0 (сетевая ошибка)
+    const url = 'https://meow-vault-pr-44.onrender.com/auth/register';
+    const body = {};
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', // Обязательно для JSON
+        },
+        body: JSON.stringify(body), // Преобразование объекта в строку
+      });
+
+      const result = await response.json();
+      console.log(result);
+      const label = result.error;
+      const message = result.message.toString();
+      this.errorService.showCustomToster(message, label, 'positive', 4000);
+    } catch (error) {
+      console.error('Ошибка запроса:', error);
+    }
+  }
+}
--- frontend/src/app/core/services/app-error-service.ts
+++ frontend/src/app/core/services/app-error-service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { TuiAlertService } from '@taiga-ui/core';
+import { TuiAlertService, TuiAppearanceOptions } from '@taiga-ui/core';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +7,24 @@ import { TuiAlertService } from '@taiga-ui/core';
 export class AppErrorService {
   private readonly alerts = inject(TuiAlertService);
 
-  public handleToster(message: string, label = 'Ошибка'): void {
-    this.alerts.open(message, { label, appearance: 'negative', autoClose: 8000 }).subscribe();
+  public showCustomToster(
+    message: string,
+    label = 'error',
+    appearance: TuiAppearanceOptions['appearance'],
+    closeTime: number,
+  ): void {
+    this.alerts.open(message, { label, appearance: appearance, autoClose: closeTime }).subscribe();
+  }
+
+  public showErrorToster(message: string, label = 'error', closeTime = 4000): void {
+    this.alerts.open(message, { label, appearance: 'negative', autoClose: closeTime }).subscribe();
+  }
+
+  public showWarningToster(message: string, label = 'warning', closeTime = 4000): void {
+    this.alerts.open(message, { label, appearance: 'warning', autoClose: closeTime }).subscribe();
+  }
+
+  public showPositiveToster(message: string, label = 'meow', closeTime = 4000): void {
+    this.alerts.open(message, { label, appearance: 'positive', autoClose: closeTime }).subscribe();
   }
 }
