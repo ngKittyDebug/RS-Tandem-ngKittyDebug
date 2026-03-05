47bfef89348f609f36a3bd21c908fa8a67b58f16
Alena1409 <166591609+Alena1409@users.noreply.github.com>
2026-03-05 09:27:54 +0400

    feat(frontend): add login page and create auth service (#36)
    * feat: add basic markup of login page
    
    * style: add new variable
    
    * feat: add auth-service
    
    * feat: add switcher button and submit-logic
    
    * feat: add cat-component and delete google-button
    
    * refactor: add api-constants and change url in auth-service
    
    * fix: change mistake with takeUntilDestroyed
    
    * refactor: add enum APP_ROUTES and change submit metod

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,24 +1,35 @@
 import { Routes } from '@angular/router';
 import { provideTranslocoScope } from '@jsverse/transloco';
 
+export enum AppRoute {
+  REGISTRATION = 'registration',
+  LOGIN = 'login',
+  USER_PROFILE = 'user-profile',
+  MAIN = '',
+}
+
+export const getRoutePath = (route: AppRoute): `/${AppRoute}` => {
+  return `/${route}`;
+};
+
 export const routes: Routes = [
   {
-    path: '',
+    path: AppRoute.MAIN,
     loadComponent: () => import('./pages/main/main').then((m) => m.Main),
     providers: [provideTranslocoScope('main')],
   },
   {
-    path: 'login',
+    path: AppRoute.LOGIN,
     loadComponent: () => import('./pages/login/login').then((m) => m.Login),
     providers: [provideTranslocoScope('login')],
   },
   {
-    path: 'registration',
+    path: AppRoute.REGISTRATION,
     loadComponent: () => import('./pages/registration/registration').then((m) => m.Registration),
     providers: [provideTranslocoScope('registration')],
   },
   {
-    path: 'user-profile',
+    path: AppRoute.USER_PROFILE,
     loadComponent: () => import('./pages/user-profile/user-profile').then((m) => m.UserProfile),
     providers: [provideTranslocoScope('user-profile')],
   },
