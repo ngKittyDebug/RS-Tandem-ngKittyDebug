6ac7f5aab588a01f42311e54043514d302d66c8e
PavelKuvshinov <pavelkuvsh1noff1989@mail.ru>
2026-03-08 08:21:21 +0300

    feat: add basic logout button

--- frontend/src/app/core/layout/header/header.ts
+++ frontend/src/app/core/layout/header/header.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { TuiHeader } from '@taiga-ui/layout';
 import { ThemeSwitcher } from '../../components/theme-switcher/theme-switcher';
-import { TuiAppearance } from '@taiga-ui/core';
+import { TuiAppearance, TuiButton } from '@taiga-ui/core';
 import { RouterLink } from '@angular/router';
 import { LaguageSwitcher } from '../../components/language-switcher/language-switcher';
+import { TuiAvatar } from '@taiga-ui/kit';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-header',
-  imports: [TuiHeader, RouterLink, TuiAppearance, LaguageSwitcher, ThemeSwitcher],
+  imports: [
+    TuiHeader,
+    RouterLink,
+    TranslocoDirective,
+    TuiAppearance,
+    TuiAvatar,
+    TuiButton,
+    LaguageSwitcher,
+    ThemeSwitcher,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
