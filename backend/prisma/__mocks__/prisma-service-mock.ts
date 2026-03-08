907e04028d3188112413035372669656e997b90d
WhaleisaJoy <afterlifelineonomega@gmail.com>
2026-03-08 23:13:00 +0300

    Revert "Development notes kozochkina82"

--- backend/prisma/__mocks__/prisma-service-mock.ts
+++
@@ -1,6 +0,0 @@
-import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
-import { PrismaClient } from 'src/generated/prisma/client';
-
-export const createMockPrismaService = (): DeepMockProxy<PrismaClient> => {
-  return mockDeep<PrismaClient>();
-};
--- frontend/transloco.config.ts
+++
@@ -1,9 +0,0 @@
-import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
-
-const config: TranslocoGlobalConfig = {
-  rootTranslationsPath: 'public/i18n/',
-  langs: ['ru', 'en'],
-  keysManager: {},
-};
-
-export default config;
