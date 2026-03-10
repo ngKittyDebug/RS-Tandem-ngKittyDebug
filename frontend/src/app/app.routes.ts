6b0fd70ab30504a69dbfcb37840f32e7d4e34574
Alena Alekseeva <a77993519@gmail.com>
2026-03-10 13:29:36 +0400

    feat: add guards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { provideTranslocoScope } from '@jsverse/transloco';
+import { guestGuard } from './core/guards/guest-guard';
+import { authGuard } from './core/guards/auth-guard';
 
 export enum AppRoute {
   REGISTRATION = 'registration',
@@ -22,16 +24,19 @@ export const routes: Routes = [
     path: AppRoute.LOGIN,
     loadComponent: () => import('./pages/login/login').then((m) => m.Login),
     providers: [provideTranslocoScope('login')],
+    canActivate: [guestGuard],
   },
   {
     path: AppRoute.REGISTRATION,
     loadComponent: () => import('./pages/registration/registration').then((m) => m.Registration),
     providers: [provideTranslocoScope('registration')],
+    canActivate: [guestGuard],
   },
   {
     path: AppRoute.USER_PROFILE,
     loadComponent: () => import('./pages/user-profile/user-profile').then((m) => m.UserProfile),
     providers: [provideTranslocoScope('user-profile')],
+    canActivate: [authGuard],
   },
   {
     path: '**',
