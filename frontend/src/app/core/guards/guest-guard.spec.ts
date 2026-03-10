9af16da09572945fd34de9d0c52b9c3253998f2c
Alena Alekseeva <a77993519@gmail.com>
2026-03-10 23:09:56 +0400

    feat: add unit test in guest-guard

--- frontend/src/app/core/guards/guest-guard.spec.ts
+++ frontend/src/app/core/guards/guest-guard.spec.ts
@@ -1,17 +1,59 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  provideRouter,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 
 import { guestGuard } from './guest-guard';
+import { AuthService } from '../services/auth/auth-service';
+import { AppRoute, getRoutePath } from '../../app.routes';
 
 describe('guestGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
     TestBed.runInInjectionContext(() => guestGuard(...guardParameters));
 
+  let authServiceMock: { isLoggedIn: () => boolean };
+  let router: Router;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authServiceMock = { isLoggedIn: (): boolean => false };
+
+    TestBed.configureTestingModule({
+      providers: [provideRouter([]), { provide: AuthService, useValue: authServiceMock }],
+    });
+
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow access if user is not logged in', () => {
+    authServiceMock.isLoggedIn = (): boolean => false;
+
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    const result = executeGuard(route, state);
+
+    expect(result).toBe(true);
+  });
+
+  it('should redirect to main if user is logged in', () => {
+    authServiceMock.isLoggedIn = (): boolean => true;
+
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    const mainPath = getRoutePath(AppRoute.MAIN);
+
+    const result = executeGuard(route, state) as UrlTree;
+
+    expect(router.serializeUrl(result)).toBe(router.serializeUrl(router.createUrlTree([mainPath])));
   });
 });
