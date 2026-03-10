99e0c5e434938c35ce39dcd50f326995198d61d5
Alena Alekseeva <a77993519@gmail.com>
2026-03-10 21:57:01 +0400

    feat: add test in auth-guards

--- frontend/src/app/core/guards/auth-guard.spec.ts
+++ frontend/src/app/core/guards/auth-guard.spec.ts
@@ -1,17 +1,61 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  provideRouter,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 
 import { authGuard } from './auth-guard';
+import { AuthService } from '../services/auth/auth-service';
+import { AppRoute, getRoutePath } from '../../app.routes';
 
 describe('authGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) =>
     TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
+  let authServiceMock: { isLoggedIn: () => boolean };
+  let router: Router;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    authServiceMock = { isLoggedIn: (): boolean => true };
+
+    TestBed.configureTestingModule({
+      providers: [provideRouter([]), { provide: AuthService, useValue: authServiceMock }],
+    });
+
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow access if user is logged in', () => {
+    authServiceMock.isLoggedIn = (): boolean => true;
+
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    const result = executeGuard(route, state);
+
+    expect(result).toBe(true);
+  });
+
+  it('should redirect to login if user is not logged in', () => {
+    authServiceMock.isLoggedIn = (): boolean => false;
+
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+
+    const loginPath = getRoutePath(AppRoute.LOGIN);
+
+    const result = executeGuard(route, state) as UrlTree;
+
+    expect(router.serializeUrl(result)).toBe(
+      router.serializeUrl(router.createUrlTree([loginPath])),
+    );
   });
 });
