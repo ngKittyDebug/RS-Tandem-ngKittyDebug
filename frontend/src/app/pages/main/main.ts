bc4c0069fc17f836d07d52c11b61bd0b2c522aee
kozochkina82 <kozochkina82@yandex.ru>
2026-03-11 19:10:08 +0100

    fix: fix styles, add button instead of link, add router link

--- frontend/src/app/pages/main/main.ts
+++ frontend/src/app/pages/main/main.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { TuiLink } from '@taiga-ui/core/components/link';
 
 @Component({
   selector: 'app-main',
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, TuiLink],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
--- frontend/src/app/pages/not-found/not-found.ts
+++ frontend/src/app/pages/not-found/not-found.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChangeDetectionStrategy } from '@angular/core';
-import { TuiLink } from '@taiga-ui/core';
+import { TuiButton } from '@taiga-ui/core';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { AppRoute, getRoutePath } from '../../app.routes';
 
 @Component({
   selector: 'app-not-found',
-  imports: [TranslocoDirective, TuiLink],
+  imports: [TranslocoDirective, TuiButton],
   templateUrl: './not-found.html',
   styleUrls: ['./not-found.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,6 +16,6 @@ import { TranslocoDirective } from '@jsverse/transloco';
 export class NotFound {
   private readonly router = inject(Router);
   public goHome(): void {
-    this.router.navigate(['/']);
+    this.router.navigate([getRoutePath(AppRoute.MAIN)]);
   }
 }
