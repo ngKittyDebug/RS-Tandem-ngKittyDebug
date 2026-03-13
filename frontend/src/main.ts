5dd01697eb920cb75e514b34717cca84e032e404
JsPowWow <js.developer.powwow@gmail.com>
2026-03-13 12:26:24 +0100

    feat(env): add NG_APP_ENV (build mode usage)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-if (import.meta.env['MODE'] === 'development') {
+if (import.meta.env['NG_APP_ENV'] === 'development') {
   console.info('[MeowVault] API_URL from env:', import.meta.env['NG_APP_API_URL']);
 }
 
