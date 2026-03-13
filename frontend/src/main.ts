1cde9cb2e5c4c5938a10bb7795c23b2efd226c4f
JsPowWow <js.developer.powwow@gmail.com>
2026-03-13 12:59:41 +0100

    feat(env): remove meta.env trace

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-console.info('[MeowVault] API_URL:', import.meta.env?.['NG_APP_API_URL']);
-console.info('[MeowVault] env:', import.meta.env);
+console.info(`[MeowVault] API_URL: ${import.meta.env?.['NG_APP_API_URL']}`);
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
