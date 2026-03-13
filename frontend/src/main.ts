e905585be794bd7d9daf58d71a1d0dd48f22b744
JsPowWow <js.developer.powwow@gmail.com>
2026-03-13 12:56:29 +0100

    feat(env): add meta.env trace

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-console.info('[MeowVault] API_URL from env:', import.meta.env['NG_APP_API_URL']);
+console.info('[MeowVault] API_URL:', import.meta.env?.['NG_APP_API_URL']);
+console.info('[MeowVault] env:', import.meta.env);
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
