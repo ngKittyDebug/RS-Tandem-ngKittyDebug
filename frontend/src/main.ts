a2ee7220b13a01021386e4d3128d91752a90f16d
JsPowWow <js.developer.powwow@gmail.com>
2026-03-13 12:33:36 +0100

    feat(env): add NG_APP_API_URL trace

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-if (import.meta.env['NG_APP_ENV'] === 'development') {
-  console.info('[MeowVault] API_URL from env:', import.meta.env['NG_APP_API_URL']);
-}
+console.info('[MeowVault] API_URL from env:', import.meta.env['NG_APP_API_URL']);
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
