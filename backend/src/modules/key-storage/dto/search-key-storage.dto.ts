6d5a1d203a47d1e4311100c0ce6e013d86edbda6
AlexGorSer <taskekaminari@gmail.com>
2026-03-15 20:58:25 +0200

    docs(https://github.com/ngKittyDebug/RS-Tandem-ngKittyDebug/issues/104): update docs

--- backend/src/modules/key-storage/dto/search-key-storage.dto.ts
+++ backend/src/modules/key-storage/dto/search-key-storage.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SearchKeyStorageDto {
+  @ApiProperty({
+    description: 'Ключ для поиска записи в хранилище',
+    example: 'user_settings',
+  })
   @IsString()
   @IsNotEmpty()
   key: string;
