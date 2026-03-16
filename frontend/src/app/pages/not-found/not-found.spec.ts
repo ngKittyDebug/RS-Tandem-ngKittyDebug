2fee1007c4a16efdb47e7ce689f5edf2605614e4
kozochkina82 <kozochkina82@yandex.ru>
2026-03-16 18:39:16 +0100

    test: add tests to page 404

--- frontend/src/app/pages/not-found/not-found.spec.ts
+++ frontend/src/app/pages/not-found/not-found.spec.ts
@@ -1,11 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { TranslocoTestingModule } from '@jsverse/transloco';
 
 import { NotFound } from './not-found';
+import { AppRoute, getRoutePath } from '../../app.routes';
 
 describe('NotFound', () => {
   let component: NotFound;
   let fixture: ComponentFixture<NotFound>;
+  const routerMock = {
+    navigate: vi.fn(),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,6 +24,7 @@ describe('NotFound', () => {
           },
         }),
       ],
+      providers: [{ provide: Router, useValue: routerMock }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotFound);
@@ -28,5 +34,17 @@ describe('NotFound', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should render the page', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+
+    expect(compiled).toBeTruthy();
+  });
+  it('should navigate to home when goHome is called', () => {
+    component.goHome();
+
+    expect(routerMock.navigate).toHaveBeenCalledWith([getRoutePath(AppRoute.MAIN)]);
   });
 });
