f026fc41e8c7c28c564100732ba6f32a63119568
Oksana Mirasova <mirasovaoksana@yandex.ru>
2026-03-24 04:30:06 +0700

    feat: add start logic for game

--- frontend/src/app/components/games/cities-game/cities-game.ts
+++ frontend/src/app/components/games/cities-game/cities-game.ts
@@ -5,6 +5,7 @@ import { TranslocoService, TranslocoModule } from '@jsverse/transloco';
 import { EyeCompassDirective } from '../../../core/directive/eye-compass.directive';
 import { TuiMessage, TuiDataListWrapper, TuiTextarea } from '@taiga-ui/kit';
 import { TuiTextfield, TuiButton } from '@taiga-ui/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cities-game',
@@ -13,12 +14,13 @@ import { TuiTextfield, TuiButton } from '@taiga-ui/core';
   imports: [
     RouterModule,
     TranslocoModule,
-    EyeCompassDirective,
     TuiMessage,
     TuiDataListWrapper,
     TuiTextfield,
     TuiTextarea,
     TuiButton,
+    CommonModule,
+    EyeCompassDirective,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -27,5 +29,30 @@ export class CitiesGame {
   private translocoService = inject(TranslocoService);
   public t(key: string): string {
     return this.translocoService.translate(key);
+  }
+  public messages: string[] = [
+    'citiesGame.messageFromCat1',
+    'citiesGame.messageFromCat2',
+    'citiesGame.messageFromCat3',
+    'citiesGame.messageFromCat4',
+    'citiesGame.messageFromCat5',
+    'citiesGame.messageFromCat6',
+    'citiesGame.messageFromCat7',
+  ];
+  public visibleMessages: string[] = [];
+  private index = 0;
+  private runMessages(): void {
+    if (this.index >= this.messages.length) return;
+
+    setTimeout(() => {
+      this.visibleMessages.push(this.messages[this.index]);
+      this.index++;
+      this.runMessages();
+    }, 2500);
+  }
+  public restart(): void {
+    this.visibleMessages = [];
+    this.index = 0;
+    this.runMessages();
   }
 }
