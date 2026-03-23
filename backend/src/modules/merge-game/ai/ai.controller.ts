bf4ac0d7ffca1819b14474ac054666baa62c0af8
Alena Alekseeva <a77993519@gmail.com>
2026-03-23 20:50:52 +0400

    refactor: add description by swager

--- backend/src/modules/merge-game/ai/ai.controller.ts
+++ backend/src/modules/merge-game/ai/ai.controller.ts
@@ -2,11 +2,28 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { CheckAnswerDto } from './dto/check-answer.dto';
 import { Public } from 'src/decorators/public.decorator';
+import { ApiBody, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Public()
 @Controller('ai')
 export class AiController {
   constructor(private readonly aiService: AiService) {}
+
+  @ApiOperation({
+    summary: 'Отправка запроса ИИ-агенту',
+    description:
+      'Отправляет запрос ИИ (Groq) на сравнение ответа пользователя и эталонного ответа.',
+  })
+  @ApiBody({ type: CheckAnswerDto })
+  @ApiOkResponse({
+    description: 'Успешный ответ',
+    schema: {
+      example: {
+        isCorrect: true,
+        feedback: 'Твой ответ соответвует эталонному',
+      },
+    },
+  })
   @Post('check-answer')
   checkAnswer(@Body() checkAnswerDto: CheckAnswerDto) {
     return this.aiService.checkAnswer(checkAnswerDto);
--- backend/src/modules/merge-game/ai/dto/check-answer.dto.ts
+++ backend/src/modules/merge-game/ai/dto/check-answer.dto.ts
@@ -1,18 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CheckAnswerDto {
+  @ApiProperty({
+    description: 'Ответ пользователя',
+    example:
+      'Метод map проходит по каждому элементу массива, применяет callback-функцию и возвращает новый массив той же длины с преобразованными элементами.',
+  })
   @IsString()
   @IsNotEmpty()
   userAnswer: string;
 
+  @ApiProperty({
+    description: 'Вопрос от игры',
+    example: 'Что делает метод map с массивом?',
+  })
   @IsString()
   @IsNotEmpty()
   question: string;
 
+  @ApiProperty({
+    description: 'Эталонный ответ',
+    example:
+      'Метод map проходит по каждому элементу массива, применяет callback-функцию и возвращает новый массив той же длины с преобразованными элементами.',
+  })
   @IsString()
   @IsNotEmpty()
   answer: string;
 
+  @ApiProperty({
+    description: 'Стиль ответа ИИ',
+    example: 'kind',
+  })
   @IsString()
   @IsNotEmpty()
   personality: string;
