84e194f61a8888b90e580b5f8aa5e400d8b4a2b8
AlexGorSer <taskekaminari@gmail.com>
2026-03-27 22:54:20 +0200

    feat: add images, base about

--- frontend/src/app/pages/about/cards/github.ts
+++ frontend/src/app/pages/about/cards/github.ts
@@ -1,33 +1,52 @@
 export const GitHubs = [
   {
     id: 1,
-    name: 'Alex',
+    name: 'AlexGorSer',
     gitHubLink: 'https://github.com/AlexGorSer',
+    imageLink: 'assets/images/gitHubProfiles/Loshara.jpg',
+    badges: ['backend', 'CI/CD'],
   },
   {
     id: 2,
-    name: 'Alex',
-    gitHubLink: 'https://github.com/AlexGorSer',
+    name: 'WhaleisaJoy',
+    gitHubLink: 'https://github.com/WhaleisaJoy',
+    imageLink: 'assets/images/gitHubProfiles/Maria.jpg',
+    badges: ['frontend', 'team-lead'],
   },
   {
     id: 3,
-    name: 'Alex',
-    gitHubLink: 'https://github.com/AlexGorSer',
+    name: 'pavelkuvsh1noff',
+    gitHubLink: 'https://github.com/pavelkuvsh1noff',
+    imageLink: 'assets/images/gitHubProfiles/Pavel.png',
+    badges: ['frontend'],
   },
   {
     id: 4,
-    name: 'Alex',
-    gitHubLink: 'https://github.com/AlexGorSer',
+    name: 'Oksi2510',
+    gitHubLink: 'https://github.com/Oksi2510',
+    imageLink: 'assets/images/gitHubProfiles/Oksana.jpg',
+    badges: ['frontend'],
   },
   {
     id: 5,
-    name: 'Alex',
-    gitHubLink: 'https://github.com/AlexGorSer',
+    name: 'Alena1409',
+    gitHubLink: 'https://github.com/Alena1409',
+    imageLink: 'assets/images/gitHubProfiles/Alena.jpg',
+    badges: ['frontend'],
   },
   {
     id: 6,
-    name: 'Alex',
-    gitHubLink: 'https://github.com/AlexGorSer',
+    name: 'kozochkina82',
+    gitHubLink: 'https://github.com/kozochkina82',
+    imageLink: 'assets/images/gitHubProfiles/Hope.png',
+    badges: ['frontend'],
+  },
+  {
+    id: 7,
+    name: 'JsPowWow',
+    gitHubLink: 'https://github.com/JsPowWow',
+    imageLink: 'assets/images/gitHubProfiles/Legend.png',
+    badges: ['legend', 'claude'],
   },
 ] satisfies GitHubData[];
 
@@ -35,4 +54,6 @@ export interface GitHubData {
   id: number;
   name: string;
   gitHubLink: string;
+  imageLink: string;
+  badges: string[] | string;
 }
--- frontend/src/app/pages/about/components/github-cards/github-cards.ts
+++ frontend/src/app/pages/about/components/github-cards/github-cards.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { GitHubData } from '../../cards/github';
 import { TranslocoDirective } from '@jsverse/transloco';
+import { TuiAvatar, TuiBadge } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-github-cards',
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, TuiAvatar, TuiBadge],
   templateUrl: './github-cards.html',
   styleUrl: './github-cards.scss',
 })
