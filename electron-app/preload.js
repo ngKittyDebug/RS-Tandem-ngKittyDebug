7274eebfc2ee604231d2181be2672d588ca2f4f2
AlexGorSer <taskekaminari@gmail.com>
2026-04-05 22:36:43 +0200

    feat: add electron

---
+++ electron-app/preload.js
@@ -0,0 +1,19 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('electron', {
+  // Здесь можно добавить API для взаимодействия между Angular и Electron
+  send: (channel, data) => {
+    const validChannels = ['toMain'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    const validChannels = ['fromMain'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  },
+});
